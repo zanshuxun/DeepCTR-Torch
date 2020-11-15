ccb87bdc096318da248d2fa8fdb882a81b89ddb2
zanshuxun <631763140@qq.com>
2020-11-15 21:49:46 +0800

    v0.2.3

--- deepctr_torch/models/dcn.py
+++ deepctr_torch/models/dcn.py
@@ -2,9 +2,11 @@
 """
 Author:
     chen_kkkk, bgasdo36977@gmail.com
+
     zanshuxun, zanshuxun@aliyun.com
 Reference:
     [1] Wang R, Fu B, Fu G, et al. Deep & cross network for ad click predictions[C]//Proceedings of the ADKDD'17. ACM, 2017: 12. (https://arxiv.org/abs/1708.05123)
+
     [2] Wang R, Shivanna R, Cheng D Z, et al. DCN-M: Improved Deep & Cross Network for Feature Cross Learning in Web-scale Learning to Rank Systems[J]. 2020. (https://arxiv.org/abs/2008.13535)
 """
 import torch
@@ -22,6 +24,7 @@ class DCN(BaseModel):
     :param linear_feature_columns: An iterable containing all the features used by linear part of the model.
     :param dnn_feature_columns: An iterable containing all the features used by deep part of the model.
     :param cross_num: positive integet,cross layer number
+    :param cross_parameterization: str, ``"vector"`` or ``"matrix"``, how to parameterize the cross network.
     :param dnn_hidden_units: list,list of positive integer or empty list, the layer number and units in each layer of DNN
     :param l2_reg_embedding: float. L2 regularizer strength applied to embedding vector
     :param l2_reg_cross: float. L2 regularizer strength applied to cross net
@@ -29,7 +32,6 @@ class DCN(BaseModel):
     :param init_std: float,to use as the initialize std of embedding vector
     :param seed: integer ,to use as random seed.
     :param dnn_dropout: float in [0,1), the probability we will drop out a given DNN coordinate.
-    :param parameterization: str, ``"vector"`` or ``"matrix"``, how to parameterize the cross network.
     :param dnn_use_bn: bool. Whether use BatchNormalization before activation or not DNN
     :param dnn_activation: Activation function to use in DNN
     :param task: str, ``"binary"`` for  binary logloss or  ``"regression"`` for regression loss
@@ -38,20 +40,14 @@ class DCN(BaseModel):
     
     """
 
-    def __init__(self,linear_feature_columns,
-                 dnn_feature_columns, cross_num=2,
-                 dnn_hidden_units=(128, 128), l2_reg_linear=0.00001,
-                 l2_reg_embedding=0.00001, l2_reg_cross=0.00001, l2_reg_dnn=0, init_std=0.0001, seed=1024,
-                 dnn_dropout=0, parameterization='vector',
-                 dnn_activation='relu', dnn_use_bn=False, task='binary', device='cpu'):
+    def __init__(self, linear_feature_columns, dnn_feature_columns, cross_num=2, cross_parameterization='vector',
+                 dnn_hidden_units=(128, 128), l2_reg_linear=0.00001, l2_reg_embedding=0.00001, l2_reg_cross=0.00001,
+                 l2_reg_dnn=0, init_std=0.0001, seed=1024, dnn_dropout=0, dnn_activation='relu', dnn_use_bn=False,
+                 task='binary', device='cpu'):
 
         super(DCN, self).__init__(linear_feature_columns=linear_feature_columns,
-                                  dnn_feature_columns=dnn_feature_columns,
-                                  dnn_hidden_units=dnn_hidden_units,
-                                  l2_reg_embedding=l2_reg_embedding, l2_reg_dnn=l2_reg_dnn, init_std=init_std,
-                                  seed=seed,
-                                  dnn_dropout=dnn_dropout, dnn_activation=dnn_activation,
-                                  task=task, device=device)
+                                  dnn_feature_columns=dnn_feature_columns, l2_reg_embedding=l2_reg_embedding,
+                                  init_std=init_std, seed=seed, task=task, device=device)
         self.dnn_hidden_units = dnn_hidden_units
         self.cross_num = cross_num
         self.dnn = DNN(self.compute_input_dim(dnn_feature_columns), dnn_hidden_units,
@@ -66,9 +62,8 @@ class DCN(BaseModel):
 
         self.dnn_linear = nn.Linear(dnn_linear_in_feature, 1, bias=False).to(
             device)
-        print('DCN parameterization:',parameterization)
         self.crossnet = CrossNet(in_features=self.compute_input_dim(dnn_feature_columns),
-                                 layer_num=cross_num, parameterization=parameterization, device=device)
+                                 layer_num=cross_num, parameterization=cross_parameterization, device=device)
         self.add_regularization_weight(
             filter(lambda x: 'weight' in x[0] and 'bn' not in x[0], self.dnn.named_parameters()), l2_reg_dnn)
         self.add_regularization_weight(self.dnn_linear.weight, l2_reg_linear)
